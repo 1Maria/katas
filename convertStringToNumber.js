15bda1e85ba402a3089bc37bbf0e9b2ffaa61755
Maria <mliberov@gmail.com>
2020-08-04 22:38:19 -0400

    Add katas for Aug 3

---
+++ convertStringToNumber.js
@@ -0,0 +1,9 @@
+var stringToNumber = function(str){
+  return parseInt(str);
+}
+
+var stringToNumber = function(str){
+  return Number(str);
+}
+
+stringToNumber("1234")
---
+++ convert_string_to_number.rb
@@ -0,0 +1,9 @@
+def string_to_number(s)
+  s.to_i
+end
+
+def string_to_number(s)
+  Integer(s)
+end
+
+string_to_number("1234")
