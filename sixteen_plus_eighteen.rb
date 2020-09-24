7bac36e04bec17a7a5547d1de7d83ab89f47ec81
Maria <mliberov@gmail.com>
2020-09-23 20:34:58 -0400

    Add more sixteen plus eighteen solutions

--- sixteen_plus_eighteen.rb
+++ sixteen_plus_eighteen.rb
@@ -1,3 +1,4 @@
+# first solution
 def silly_add(a, b)
   max_num = [a, b].max
   min_num = [a, b].min
@@ -7,6 +8,25 @@ def silly_add(a, b)
 
   first_array.each_index {|i| sum = ((first_array[i] ||= 0) + (second_array[i] ||= 0)).to_s + sum }
   return sum.to_i
+end
+
+# refactor
+def silly_add(a, b)
+  first_array = [a, b].max.digits
+  second_array = [a, b].min.digits
+  sum = '';
+
+  first_array.each_index {|i| sum = ((first_array[i] ||= 0) + (second_array[i] ||= 0)).to_s + sum }
+  return sum.to_i
+end
+
+# second solution
+def silly_add(a, b)
+  larger_array = [a, b].max.digits
+  smaller_array = [a, b].min.digits
+
+  smaller_array << 0 until larger_array.size == smaller_array.size
+  [smaller_array.reverse, larger_array.reverse].transpose.map {|x| x.reduce(:+)}.join.to_i
 end
 
 puts silly_add(16, 18)      # 214
